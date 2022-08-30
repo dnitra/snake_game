a09c6f7b28ed062e00f9c572dbd2e8329dc15413
dnitra <109307365+dnitra@users.noreply.github.com>
2022-08-30 22:08:44 +0200

    improved UI

--- script.js
+++ script.js
@@ -6,29 +6,21 @@ const mapSize = document.querySelector("#mapSize")
 const totalLayoutSlider = document.querySelector("#totalPixels")
 const gameSpeedSlider = document.querySelector("#gameSpeed")
 const watchBotSlider = document.querySelector("#watchBot")
+const twoPlayersButton = document.querySelector("#twoPlayers")
 
 
-
-
-const helpButton =document.querySelector("#help-content") 
 const user = window.mobileAndTabletCheck = function() {
     let check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
   };
-/*
-if(user()){ 
-    helpButton.innerHTML = 
-    `<p>Click on size button to change the layout!</p> 
-    <p>Use display to navigate the snake!</p> 
-    <img src='display_touch_area.png'>`
+
+if(user()){
+   
+    startGame.style.display="none"
+    twoPlayersButton.style.display="none"
 }
-else{
-    helpButton.innerHTML = 
-    `<p>Click on size button to change the layout!</p>
-    <p>Use keyboard arrows to navigate the snake!</p>`
-}
-*/
+ 
 const restartButton = document.createElement("button") 
 restartButton.id = "restartButton"
 restartButton.textContent = "Restart"
@@ -59,10 +51,10 @@ let barrier = 20
 
 let gameSpeed = gameSpeedSlider.value
 let gamerPixels = mapSize.value
-let gamerMode = moveRight
+let playerOneMode = moveRight
 
 let count=0
-let gamerFocus = 0
+let gamerFocus = 1
 let botFocus =0
 let gameOver = true
 let gameStart = false
@@ -71,26 +63,30 @@ let watchBot=true
 
 startGame.addEventListener("click",()=>{
     watchBot=false
-    allSnakes.unshift(createSnake())  
     gameOver=false
     gameStart=true
     botFocus=1
     gamerFocus = 0
-
+    playerOneMode=moveRight
+    clearInterval(game)
+    playGame()
+    allSnakes.unshift(createSnake())  
 })
 
 window.addEventListener("keydown",keyPressed)
-mapSize.addEventListener("input",(e)=>{gamerPixels = e.target.value})
+mapSize.addEventListener("input",(e)=>gamerPixels = e.target.min*e.target.max/ e.target.value)
 watchBotSlider.addEventListener("click",(e)=>{
     watchBot=true
     gameOver=false
     gameStart=true
     botFocus=0
-    gamerFocus = random(0,allSnakes.length)
+    gamerFocus = 1
+    clearInterval(game)
+    playGame()
   
 })
 gameSpeedSlider.addEventListener("input",(e)=>{
-    gameSpeed = e.target.value
+    gameSpeed = Math.round(5000/e.target.value)
     clearInterval(game)
     playGame()
 })
@@ -119,7 +115,7 @@ totalLayoutSlider.addEventListener("mouseup",()=>{
 
 //restart////////////////////
 
-playGame()
+//playGame()
 totalLayout(totalPixels, gameArray)
 
 
@@ -135,36 +131,30 @@ for(let i=0;i<initialSnakes;i++){
 function playGame(){
    
     game = setInterval(()=>{
-    if(allSnakes.length<1){
-        clearInterval(game)
-    }
-    count++
-    
-    score = allSnakes[gamerFocus].length-3
-    document.querySelector("#score").textContent = "Score: "+ score
+        if(allSnakes.length>0){
+            count++
+            if(allSnakes.length==1){
+                gamerFocus=0
+            }
 
-   
-    
-    if(!gameOver){
-        if(!watchBot){
+            score = allSnakes[gamerFocus].length-3
+            document.querySelector("#score").textContent = "Score: "+ score
+            if(!gameOver){
+                if(!watchBot){
+                    
+                    snakeMovement(allSnakes[0],playerOneMode)
+                }
+            }
+            for(let i=botFocus;i<allSnakes.length;i++){
+                botSnakeMovement(allSnakes[i])
+            }
+           
             
-            snakeMovement(allSnakes[0],gamerMode)
-        }
-
-    
-   
+            gamerLayout(allSnakes[gamerFocus][allSnakes[gamerFocus].length-1])     
+        }  
     }
-    for(let i=botFocus;i<allSnakes.length;i++){
-        botSnakeMovement(allSnakes[i])
-    }
-    /*
-    for(let i=0;i<foodArray.length;i++){
-        generatePathToFood(foodArray[i])
-    }
-    */
-    
-    gamerLayout(allSnakes[gamerFocus][allSnakes[gamerFocus].length-1])     
-      }, gameSpeed)
+      
+      ,gameSpeed)
 }
 
 function totalLayout(pixels,arr){
@@ -218,8 +208,8 @@ function gamerLayout(snakeHead){
     let heightGame = container_game.offsetHeight
     let ratio = widthGame/heightGame
     
-    pixelsX =Math.round(Math.sqrt(ratio *gamerPixels))
-    pixelsY = Math.round((gamerPixels/pixelsX))
+    pixelsX =Math.round(Math.sqrt(ratio *gamerPixels*gamerPixels))
+    pixelsY = Math.round((gamerPixels*gamerPixels/pixelsX))
 
     let startIndex_X = Math.round(snakeHead[0]-pixelsY/2)
     let startIndex_Y = Math.round(snakeHead[1]-pixelsX/2)
@@ -241,8 +231,9 @@ function gamerLayout(snakeHead){
                 min-height:${heightGame/pixelsY}px;
                 `
             
+            
            /* 
-            if(gamerMode == moveLeft||gamerMode == moveRight){
+            if(playerOneMode == moveLeft||playerOneMode == moveRight){
                 if(x<=pixelsY/2){
                     square.classList.add("ArrowUp")
                 }
@@ -250,7 +241,7 @@ function gamerLayout(snakeHead){
                     square.classList.add("ArrowDown")
                 }
             }
-            if(gamerMode == moveUp||gamerMode == moveDown){
+            if(playerOneMode == moveUp||playerOneMode == moveDown){
                 if(y<pixelsX/2){
                     square.classList.add("ArrowLeft")
                 }
@@ -263,23 +254,18 @@ function gamerLayout(snakeHead){
                 //square.textContent = pathArray[x+startIndex_X][y+startIndex_Y]
 
                 if(gameArray[x+startIndex_X][y+startIndex_Y] !=undefined){
-                        square.classList.add(`${gameArray[x+startIndex_X][y+startIndex_Y]}`)
-                        container_game.appendChild(square)
+                    square.classList.add(`${gameArray[x+startIndex_X][y+startIndex_Y]}`)
+                    container_game.appendChild(square)
                 }
             }
         }
     } 
     
-    document.querySelectorAll(".ArrowDown").forEach(arrow => arrow.addEventListener("mousedown",()=>{if(gamerMode!=moveUp){gamerMode= moveDown}}));
-    document.querySelectorAll(".ArrowLeft").forEach(arrow => arrow.addEventListener("mousedown",()=>{if(gamerMode!=moveRight){gamerMode= moveLeft}}));
-    document.querySelectorAll(".ArrowRight").forEach(arrow => arrow.addEventListener("mousedown",()=>{if(gamerMode!=moveLeft){gamerMode= moveRight}}));
-    document.querySelectorAll(".ArrowUp").forEach(arrow => arrow.addEventListener("mousedown",()=>{if(gamerMode!=moveDown){gamerMode= moveUp}}));
-    document.querySelectorAll(".square").forEach(arrow => arrow.addEventListener("mousedown",()=>{
-        if(!gameStart){
-            gameStart = true
-            playGame()
-        }
-    }));
+    document.querySelectorAll(".ArrowDown").forEach(arrow => arrow.addEventListener("mousedown",()=>{if(playerOneMode!=moveUp){playerOneMode= moveDown}}));
+    document.querySelectorAll(".ArrowLeft").forEach(arrow => arrow.addEventListener("mousedown",()=>{if(playerOneMode!=moveRight){playerOneMode= moveLeft}}));
+    document.querySelectorAll(".ArrowRight").forEach(arrow => arrow.addEventListener("mousedown",()=>{if(playerOneMode!=moveLeft){playerOneMode= moveRight}}));
+    document.querySelectorAll(".ArrowUp").forEach(arrow => arrow.addEventListener("mousedown",()=>{if(playerOneMode!=moveDown){playerOneMode= moveUp}}));
+    
 }
 function random (min, max) {return Math.floor(Math.random() * (max - min)) + min}
 
@@ -318,19 +304,26 @@ function snakeMovement(snake, mode){
     
 
     if(newSnakeHead[0]>totalPixels-2 || newSnakeHead[0]<1 || newSnakeHead[1]>totalPixels-2 || newSnakeHead[1]<1||gameArray[newSnakeHead[0]][newSnakeHead[1]]=="snake"||gameArray[newSnakeHead[0]][newSnakeHead[1]]=="head"){
-        
+        if(allSnakes.length==1){
+            gamerFocus=0
+        }
         for(let key of snake){
             gameArray[key[0]].splice(key[1],1,"snakeFood")
             pathArray[key[0]].splice(key[1],1,botVisibility)
             generatePathToFood([key[0],key[1]])
         }
         
-        gamerFocus=random(0,allSnakes.length)
+        if(gameStart&&checkSnake(snakeHead)==0){          
+            watchBot = true
+            gameOver=true
+            botFocus=0
+            gameStart=false
+            
+        }
         deleteSnake(snakeHead)
-        allSnakes.push(createSnake())
 
-        
         isSnakeDead = true
+        
             
     }
     snake.shift()
@@ -473,44 +466,61 @@ function foodApear() {
 }
   
 function restart(){
-    if(gameOver){
-        let node = document.getElementById("restartButton");
-        if (node.parentNode) {
-        node.parentNode.removeChild(node);
-        }
-    }
+    
 }
 
 function keyPressed (key){
     
-    switch(key.key){
-        case "ArrowDown":
-            if(gamerMode!=moveUp){
-                gamerMode= moveDown
+    switch(key.key.toLowerCase()){
+        case "arrowdown":
+            if(playerOneMode!=moveUp){
+                playerOneMode= moveDown
             }
             break;
-        case "ArrowUp":
-            if(gamerMode!=moveDown){
-                gamerMode= moveUp
+        case "arrowup":
+            if(playerOneMode!=moveDown){
+                playerOneMode= moveUp
             }
             break;
-        case "ArrowLeft":
-            if(gamerMode!=moveRight){
-                gamerMode= moveLeft
+        case "arrowleft":
+            if(playerOneMode!=moveRight){
+                playerOneMode= moveLeft
              }
             break;
-        case "ArrowRight":
-            if(gamerMode!=moveLeft){
-                gamerMode= moveRight
+        case "arrowright":
+            if(playerOneMode!=moveLeft){
+                playerOneMode= moveRight
             }
             break;
-        case "Escape":
+        case "escape":
             clearInterval(game)
             break;
-        case "Enter":
+        case "enter":
+            clearInterval(game)
             playGame()
             break;
+            case "s":
+                if(playerOneMode!=moveUp){
+                    gamerMode= moveDown
+                }
+                break;
+            case "w":
+                if(playerOneMode!=moveDown){
+                    playerOneMode= moveUp
+                }
+                break;
+            case "a":
+                if(playerOneMode!=moveRight){
+                    playerOneMode= moveLeft
+                 }
+                break;
+            case "d":
+                if(playerOneMode!=moveLeft){
+                    playerOneMode= moveRight
+                }
+                break;
     }
+    
 }
 
 function generatePathToFood ([foodX,foodY]){
